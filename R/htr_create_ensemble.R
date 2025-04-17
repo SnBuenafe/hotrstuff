4390246659cbefa3c50e1eebe31c6ac7c4122948
SnBuenafe <tinbuenafe@gmail.com>
2025-04-17 12:14:04 +1000

    Update htr_create_ensemble.R

--- R/htr_create_ensemble.R
+++ R/htr_create_ensemble.R
@@ -13,6 +13,7 @@
 #' @examples
 #' \dontrun{
 #' htr_create_ensemble(
+#' hpc = NA,
 #' indir = file.path(base_dir, "data", "proc", "regridded", "yearly", "tos"),
 #' outdir = file.path(base_dir, "data", "proc", "ensemble", "mean", "tos"),
 #' model_list = c("ACCESS-ESM1-5", "CanESM5"),
@@ -22,7 +23,8 @@
 #' mean = TRUE
 #' )
 #' }
-htr_create_ensemble <- function(indir,
+htr_create_ensemble <- function(hpc = NA, # if ran in the HPC, possible values are "array", "parallel"
+                                indir,
                                 outdir,
                                 model_list,
                                 variable = "tos",
@@ -36,37 +38,61 @@ htr_create_ensemble <- function(indir,
   # Create output folder if it doesn't exist
   htr_make_folder(outdir)
 
-  w <- parallel::detectCores() - 2
+  # Define workers
+  if(!is.na(hpc)) {
+    w <- parallelly::availableCores(methods = "Slurm", omit = 2)
+  } else {
+    w <- parallelly::availableCores(methods = "system", omit = 2)
+  }
 
-  if(stringr::str_length(domain) > 0 & stringr::str_length(season) > 0) {
+  ##############
+
+  if (stringr::str_length(domain) > 0 & stringr::str_length(season) > 0) {
+
     files <- dir(indir, full.names = TRUE) %>%
       stringr::str_subset(paste0("(?=.*", variable, "_", ")(?=.*", freq, "_", ")(?=.*", scenario, "_", ")(?=.*", season, "_", ")(?=.*", domain, ")")) %>%
       stringr::str_subset(paste(model_list, collapse = "|"))
-  } else if(stringr::str_length(domain) > 0) {
+
+  } else if (stringr::str_length(domain) > 0) {
+
     files <- dir(indir, full.names = TRUE) %>%
       stringr::str_subset(paste0("(?=.*", variable, "_", ")(?=.*", freq, "_", ")(?=.*", scenario, "_", ")(?=.*",  domain, ")")) %>%
       stringr::str_subset(paste(model_list, collapse = "|"))
-  } else if(stringr::str_length(season) > 0) {
+
+  } else if (stringr::str_length(season) > 0) {
+
     files <- dir(indir, full.names = TRUE) %>%
       stringr::str_subset(paste0("(?=.*", variable, "_", ")(?=.*", freq, "_", ")(?=.*", scenario, "_", ")(?=.*",  season, ")")) %>%
       stringr::str_subset(paste(model_list, collapse = "|"))
+
   } else {
+
     files <- dir(indir, full.names = TRUE) %>%
       stringr::str_subset(paste0("(?=.*", variable, "_", ")(?=.*", freq, "_", ")(?=.*", scenario, ")")) %>%
       stringr::str_subset(paste(model_list, collapse = "|"))
+
   }
 
   out_name <- files[1] %>%
     stringr::str_replace(indir, outdir) %>%
     stringr::str_replace(htr_get_CMIP6_bits(files[1])$Model, "ensemble")
 
+  ##############
+
   if (mean == TRUE) {
+
     cdo_code <- paste0("cdo -L -z zip -ensmean ", paste0(files, collapse = " "), " ", out_name)
+
   } else if (mean == FALSE) { # Calculate the median
+
     cdo_code <- paste0("cdo -L -z zip -ensmedian ", paste0(files, collapse = " "), " ", out_name)
+
   } else {
+
     print("Please provide the right option for mean")
+
   }
 
   system(cdo_code)
+
 }
